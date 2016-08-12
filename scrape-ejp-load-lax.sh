60127864ca838808e389edc93314ba32e32c6023
Luke Skibinski <l.skibinski@elifesciences.org>
2016-08-12 16:19:42 +0100

    fixed up the scrape logic a little to exit early if no report found

--- scrape-ejp-load-lax.sh
+++ scrape-ejp-load-lax.sh
@@ -8,10 +8,16 @@ if [ ! -d /opt/elife-reporting/ ]; then
     exit 0
 fi;
 
+if [ ! -e /tmp/ejp-report.json ]; then
+    echo "/tmp/ejp-report.json not found. cannot scrape"
+    exit 1
+fi
+
+cd /opt/ejp-lax-adaptor/
+
 . ./install.sh
 
 # scrape the report into json
-cd /opt/ejp-lax-adaptor/
 report="/opt/elife-reporting/paper_history$(date "+%Y_%m_%d").csv"
 python ./src/ejp_scraper.py $report > /tmp/ejp-report.json
 
