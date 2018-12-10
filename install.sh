4ebd3e6459e0968c316086b1d64947412516cf07
Luke <lsh-0@users.noreply.github.com>
2018-12-10 12:41:34 +1030

    replaced old scraper with new scraper (et3) updated install script and requirements

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e # everything must succeed.
-if [ ! -d venv ]; then
-    virtualenv --python=`which python2` venv
+if [ ! -e venv/bin/activate ]; then
+    rm -rf venv/
+    python -m venv venv
 fi
 source venv/bin/activate
-
-pip install -r requirements.txt
+pip install -r requirements.lock
--- src/ejp_scraper.py
+++ src/ejp_scraper.py
@@ -1,23 +1,13 @@
 from datetime import datetime
 from collections import OrderedDict
-import sys,csv,fileinput,json
-import logging
-from scraper.utils import fattrs
-
-LOG = logging.getLogger(__name__)
-
-def csv_report():
-    for row in csv.DictReader(fileinput.input(mode='rb'), delimiter=','):
-        yield row
+import csv, fileinput, json
+from et3.extract import path as p
+from et3.render import render
 
 def todt(ymdstr):
     if not ymdstr.strip():
         return None
     return datetime.strptime(ymdstr, "%Y-%m-%d")
-
-@fattrs('this as row')
-def debug(row):
-    print 'debug:',row
 
 mapping = dict([
     ('RJI', 'Reject Initial Submission'),
@@ -36,7 +26,6 @@ def tfield(val):
     assert val in mapping.values(), "unknown decision type %r" % val
     return mapping_rev[val]
 
-@fattrs('this.type as mstype')
 def paper_type(mstype):
     # these labels aren't actually used
     # the value is passed through so long as it exists in map
@@ -54,57 +43,47 @@ def paper_type(mstype):
     assert mstype in types.keys(), "unknown paper type %r" % mstype
     return mstype
 
-@fattrs('this.ms as msid')
-def msid(msid):
-    int(msid)
-    return msid
+DESCRIPTION = OrderedDict([
+    ('manuscript_id', [p('ms'), int, str]), # yes, str->int->str, I know.
 
-DESCRIPTION = [
-    ('article', {
-        'iterable': csv_report,
-        'attrs': OrderedDict([
-            #('debug', 'debug'),
-        
-            ('manuscript_id', 'msid'),
+    ('ejp_type', [p('type'), paper_type]),
+    
+    ('date_initial_qc', [p('initial_qc_dt', None), todt]),
+    ('date_initial_decision', [p('initial_decision_dt', None), todt]),
+    ('initial_decision', [p('initial_decision', None), tfield]),
+    
+    ('date_full_qc', [p('full_qc_dt', None), todt]),
+    ('date_full_decision', [p('full_decision_dt', None), todt]),
+    ('decision', [p('full_decision', None), tfield]),
 
-            ('ejp_type', 'paper_type'),
-            
-            ('date_initial_qc', ('this.initial_qc_dt', None, todt)),
-            ('date_initial_decision', ('this.initial_decision_dt', None, todt)),
-            ('initial_decision', ('this.initial_decision', None, tfield)),
-            
-            ('date_full_qc', ('this.full_qc_dt', None, todt)),
-            ('date_full_decision', ('this.full_decision_dt', None, todt)),
-            ('decision', ('this.full_decision', None, tfield)),
+    ('date_rev1_qc', [p('rev1_qc_dt', None), todt]),
+    ('date_rev1_decision', [p('rev1_decision_dt', None), todt]),
+    ('rev1_decision', [p('rev1_decision', None), tfield]),
 
-            ('date_rev1_qc', ('this.rev1_qc_dt', None, todt)),
-            ('date_rev1_decision', ('this.rev1_decision_dt', None, todt)),
-            ('rev1_decision', ('this.rev1_decision', None, tfield)),
+    ('date_rev2_qc', [p('rev2_qc_dt', None), todt]),
+    ('date_rev2_decision', [p('rev2_decision_dt', None), todt]),
+    ('rev2_decision', [p('rev2_decision', None), tfield]),
 
-            ('date_rev2_qc', ('this.rev2_qc_dt', None, todt)),
-            ('date_rev2_decision', ('this.rev2_decision_dt', None, todt)),
-            ('rev2_decision', ('this.rev2_decision', None, tfield)),
+    ('date_rev3_qc', [p('rev3_qc_dt', None), todt]),
+    ('date_rev3_decision', [p('rev3_decision_dt', None), todt]),
+    ('rev3_decision', [p('rev3_decision', None), tfield]),
 
-            ('date_rev3_qc', ('this.rev3_qc_dt', None, todt)),
-            ('date_rev3_decision', ('this.rev3_decision_dt', None, todt)),
-            ('rev3_decision', ('this.rev3_decision', None, tfield)),
+    ('date_rev4_qc', [p('rev4_qc_dt', None), todt]),
+    ('date_rev4_decision', [p('rev4_decision_dt', None), todt]),
+    ('rev4_decision', [p('rev4_decision', None), tfield]),
+])
 
-            ('date_rev4_qc', ('this.rev4_qc_dt', None, todt)),
-            ('date_rev4_decision', ('this.rev4_decision_dt', None, todt)),
-            ('rev4_decision', ('this.rev4_decision', None, tfield)),
-        ])
-    }),
-]
-
-def scrape():
-    import scraper
-    using_me = __import__(__name__)
-    return scraper.scrape(using_me)
+def scrape(fh):
+    return render(DESCRIPTION, csv.DictReader(fh))
 
 def customserializations(obj):
     if isinstance(obj, datetime):
         return obj.isoformat()
     raise TypeError("Type %r not serializable" % type(obj))
 
+def main(fh):
+    data = list(scrape(fh))
+    return json.dumps(data, indent=4, default=customserializations)
+
 if __name__ == '__main__':
-    print json.dumps(scrape()[0]['article'], indent=4, default=customserializations)
+    print(main(fileinput.input(mode='r')))
