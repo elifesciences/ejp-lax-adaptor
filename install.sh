90f5362e3b1f94734d7360c05737dbfdec33f99a
Luke <lsh-0@users.noreply.github.com>
2018-12-11 10:38:24 +1030

    typo, renamed 'python' to 'python3' in install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e # everything must succeed.
 rm -rf venv/
 if [ ! -e venv3/bin/activate ]; then
     rm -rf venv3/
-    python -m venv venv3
+    python3 -m venv venv3
 fi
 source venv3/bin/activate
 pip install -r requirements.lock
